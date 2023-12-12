45f4653a6527b3f6f97c74c928db35d6747ab538
sprak18 <mksurya1808@gmail.com>
2023-12-12 09:07:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [step, setStep] = useState(0);
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Step step={step} setStep={setStep} />
+      <Count count={count} setCount={setCount} step={step} />
+      <Message count={count} />
+    </>
+  );
+}
+
+function Step({ step, setStep }) {
+  return (
+    <div className="row">
+      <button onClick={() => setStep(step - 1)} className="btn">
+        -
+      </button>
+      <h2>Step: {step}</h2>
+      <button onClick={() => setStep(step + 1)} className="btn">
+        +
+      </button>
     </div>
   );
 }
 
-export default App;
+function Count({ count, setCount, step }) {
+  return (
+    <div className="row">
+      <button onClick={() => setCount(count - step)} className="btn">
+        -
+      </button>
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount(count + step)} className="btn">
+        +
+      </button>
+    </div>
+  );
+}
+
+function Message({ count }) {
+  let today = new Date();
+  today.setDate(today.getDate() + count);
+
+  if (count === 0) {
+    return <h2 className="row">{`Today is ${today.toDateString()}`}</h2>;
+  } else if (count >= 0) {
+    return (
+      <h2 className="row">{`${count} days from today is ${today.toDateString()}`}</h2>
+    );
+  } else
+    return (
+      <h2 className="row">{`${Math.abs(
+        count
+      )} days ago was ${today.toDateString()}`}</h2>
+    );
+}
